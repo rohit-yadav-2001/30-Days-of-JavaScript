a68b2d2c456b29b73d29c0621065f8d2313348e8
rohit-yadav-2001 <14rohityadav32@gmail.com>
2024-02-14 12:18:02 +0530

    first commit

---
+++ First Day.js
@@ -0,0 +1,57 @@
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/*comments can make code readable, easy to reuse and informative*/
+// Variable.js
+let firstName = "Hello, world!";
+let boolean = false;
+let undefinedVariable;
+let nullValue = null;
+
+console.log(firstName);
+console.log(boolean);
+console.log(undefinedVariable);
+console.log(nullValue);
+// answer five
+// datatype
+console.log(typeof firstName);
+console.log(typeof false);
+console.log(typeof undefinedVariable);
+console.log(typeof nullValue);
+console.log(typeof 5);
+console.log(typeof [1, 1, 1, 3]);
+// answer six
+let Name;
+let lastName;
+let country;
+let age;
+console.log(Name, lastName, country, age);
+// answer seven
+let address = "unknown";
+let study = 10;
+let pass = true;
+let school = "mit";
+console.log(address, study, pass, school);
+// answer eight
+let first_Name = "john",
+  last_name = "wick",
+  marital_Status = "unmarried",
+  country_name = "USA",
+  about_Age = "28";
+
+  console.log(first_Name);
+  console.log( last_name);
+  console.log( marital_Status);
+  console.log(country_name);
+  console.log(about_Age);
+// answer nine
+let first_name = "john",
+  last_Name = "wick",
+  marital_status = "unmarried",
+  country_Name = "USA",
+  about_age = "28";
+console.log(first_name, last_Name, marital_status, country_Name, about_age);
+// answer ten
+let myAge = 25;
+let yourAge = 30;
+console.log("I am " + myAge + " years old");
+console.log("you are " + yourAge + " years old");
